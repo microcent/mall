fe3a95b8da29f07fc4349b2f105ff43369461131
microcent <1287994577@qq.com>
2018-08-15 18:32:06 +0800

    add wx mall project

--- wx-mall/pages/about/index.js
+++ wx-mall/pages/about/index.js
@@ -4,9 +4,31 @@ const app = getApp()
 
 Page({
   data: {
-    
+    shopInfo: {
+      name: 'mall',
+      address: 'https://github.com/linlinjava/litemall',
+      latitude: 31.201900,
+      longitude: 121.587839,
+      linkPhone: '18617333732',
+      qqNumber: '738696120'
+    }
   },
-  onLoad: function () {
-    
+  onLoad: function() {
+
+  },
+  showLocation: function(e) {
+    var that = this
+    wx.openLocation({
+      latitude: that.data.shopInfo.latitude,
+      longitude: that.data.shopInfo.longitude,
+      name: that.data.shopInfo.name,
+      address: that.data.shopInfo.address,
+    })
+  },
+  callPhone: function(e) {
+    var that = this
+    wx.makePhoneCall({
+      phoneNumber: that.data.shopInfo.linkPhone,
+    })
   }
 })
